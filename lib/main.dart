e1c4b6d28d557ba55b7dd52edba2d67d084d9e41
fffussel <lucasschladitz@gmail.com>
2026-08-30 00:26:44 +0200

    wie funktioniert der dreck?

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'mainLayout.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +11,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(
+        body: Center(
+          child: Mainlayout(),
+        )
+      ),
     );
   }
 }
